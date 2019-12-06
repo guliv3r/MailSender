a736221cf417928fd4037cc403ea157bc6932086
Albert Giulbaziani <albert.giulbaziani@lb.ge>
2019-12-06 13:31:07 +0400

    add solution

---
+++ New Employee Mail Sender/Form1.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace New_Employee_Mail_Sender
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.inp_fullname = new System.Windows.Forms.TextBox();
+            this.inp_position = new System.Windows.Forms.TextBox();
+            this.inp_department = new System.Windows.Forms.TextBox();
+            this.inp_boss = new System.Windows.Forms.TextBox();
+            this.inp_email = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.inp_startDate = new System.Windows.Forms.DateTimePicker();
+            this.label8 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.inp_welcome_text = new System.Windows.Forms.RichTextBox();
+            this.btn_view_template = new System.Windows.Forms.Button();
+            this.btn_submit = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.inp_ganyofileba = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.btn_upload = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.inp_birthDay = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "სახელი გვარი:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "პოზიცია:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "დეპარტამენტი:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(35, 196);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "ხელმძღვანელი:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(35, 288);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Email:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(35, 326);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(109, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "დაწყების თარიღი:";
+            // 
+            // inp_fullname
+            // 
+            this.inp_fullname.Location = new System.Drawing.Point(153, 41);
+            this.inp_fullname.Name = "inp_fullname";
+            this.inp_fullname.Size = new System.Drawing.Size(294, 20);
+            this.inp_fullname.TabIndex = 6;
+            // 
+            // inp_position
+            // 
+            this.inp_position.Location = new System.Drawing.Point(153, 82);
+            this.inp_position.Name = "inp_position";
+            this.inp_position.Size = new System.Drawing.Size(294, 20);
+            this.inp_position.TabIndex = 7;
+            // 
+            // inp_department
+            // 
+            this.inp_department.Location = new System.Drawing.Point(153, 151);
+            this.inp_department.Name = "inp_department";
+            this.inp_department.Size = new System.Drawing.Size(294, 20);
+            this.inp_department.TabIndex = 8;
+            // 
+            // inp_boss
+            // 
+            this.inp_boss.Location = new System.Drawing.Point(153, 189);
+            this.inp_boss.Name = "inp_boss";
+            this.inp_boss.Size = new System.Drawing.Size(294, 20);
+            this.inp_boss.TabIndex = 9;
+            // 
+            // inp_email
+            // 
+            this.inp_email.Location = new System.Drawing.Point(153, 281);
+            this.inp_email.Name = "inp_email";
+            this.inp_email.Size = new System.Drawing.Size(294, 20);
+            this.inp_email.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(35, 380);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(48, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "ტექსტი:";
+            // 
+            // inp_startDate
+            // 
+            this.inp_startDate.Location = new System.Drawing.Point(153, 318);
+            this.inp_startDate.Name = "inp_startDate";
+            this.inp_startDate.Size = new System.Drawing.Size(294, 20);
+            this.inp_startDate.TabIndex = 12;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(542, 58);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(54, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "სურათი:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(545, 100);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(234, 203);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            // 
+            // inp_welcome_text
+            // 
+            this.inp_welcome_text.Location = new System.Drawing.Point(144, 377);
+            this.inp_welcome_text.Name = "inp_welcome_text";
+            this.inp_welcome_text.Size = new System.Drawing.Size(838, 133);
+            this.inp_welcome_text.TabIndex = 13;
+            this.inp_welcome_text.Text = "";
+            // 
+            // btn_view_template
+            // 
+            this.btn_view_template.Location = new System.Drawing.Point(38, 550);
+            this.btn_view_template.Name = "btn_view_template";
+            this.btn_view_template.Size = new System.Drawing.Size(184, 42);
+            this.btn_view_template.TabIndex = 18;
+            this.btn_view_template.Text = "View Template In Browser";
+            this.btn_view_template.UseVisualStyleBackColor = true;
+            this.btn_view_template.Click += new System.EventHandler(this.btn_view_template_Click);
+            // 
+            // btn_submit
+            // 
+            this.btn_submit.Location = new System.Drawing.Point(872, 550);
+            this.btn_submit.Name = "btn_submit";
+            this.btn_submit.Size = new System.Drawing.Size(110, 42);
+            this.btn_submit.TabIndex = 19;
+            this.btn_submit.Text = "გაგზავნა";
+            this.btn_submit.UseVisualStyleBackColor = true;
+            this.btn_submit.Click += new System.EventHandler(this.btn_submit_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // inp_ganyofileba
+            // 
+            this.inp_ganyofileba.Location = new System.Drawing.Point(153, 235);
+            this.inp_ganyofileba.Name = "inp_ganyofileba";
+            this.inp_ganyofileba.Size = new System.Drawing.Size(294, 20);
+            this.inp_ganyofileba.TabIndex = 10;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(35, 242);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(88, 13);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "განყოფილება:";
+            // 
+            // btn_upload
+            // 
+            this.btn_upload.Location = new System.Drawing.Point(603, 48);
+            this.btn_upload.Name = "btn_upload";
+            this.btn_upload.Size = new System.Drawing.Size(75, 23);
+            this.btn_upload.TabIndex = 21;
+            this.btn_upload.Text = "ატვირთვა";
+            this.btn_upload.UseVisualStyleBackColor = true;
+            this.btn_upload.Click += new System.EventHandler(this.btn_upload_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(35, 121);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(119, 13);
+            this.label10.TabIndex = 22;
+            this.label10.Text = "დაბადების თარიღი:";
+            // 
+            // inp_birthDay
+            // 
+            this.inp_birthDay.Location = new System.Drawing.Point(153, 115);
+            this.inp_birthDay.Name = "inp_birthDay";
+            this.inp_birthDay.Size = new System.Drawing.Size(294, 20);
+            this.inp_birthDay.TabIndex = 23;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1045, 640);
+            this.Controls.Add(this.inp_birthDay);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.btn_upload);
+            this.Controls.Add(this.inp_ganyofileba);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.btn_submit);
+            this.Controls.Add(this.btn_view_template);
+            this.Controls.Add(this.inp_welcome_text);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.inp_startDate);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.inp_email);
+            this.Controls.Add(this.inp_boss);
+            this.Controls.Add(this.inp_department);
+            this.Controls.Add(this.inp_position);
+            this.Controls.Add(this.inp_fullname);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Mail Sender (New Employee)";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox inp_fullname;
+        private System.Windows.Forms.TextBox inp_position;
+        private System.Windows.Forms.TextBox inp_department;
+        private System.Windows.Forms.TextBox inp_boss;
+        private System.Windows.Forms.TextBox inp_email;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DateTimePicker inp_startDate;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox inp_welcome_text;
+        private System.Windows.Forms.Button btn_view_template;
+        private System.Windows.Forms.Button btn_submit;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox inp_ganyofileba;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button btn_upload;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.DateTimePicker inp_birthDay;
+    }
+}
+
---
+++ New Employee Mail Sender/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace New_Employee_Mail_Sender
+{
+    public partial class Form1 : Form
+    {
+        private string imageStr;
+        private string body;
+        public Form1()
+        {
+            InitializeComponent();
+            imageStr = string.Empty;
+            body = string.Empty;
+            System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("ka-GE");
+            //System.Globalization.CultureInfo ci = new System.Globalization.CultureInfo("ka-GE");
+            //inp_startDate.Format = DateTimePickerFormat.Custom;
+            //inp_startDate.CustomFormat = ci.DateTimeFormat.ShortDatePattern;
+        }
+
+        public void FillHtml()
+        {
+            if (string.IsNullOrEmpty(imageStr))
+            {
+                MessageBox.Show("სურათის ატვირთვა სავალდებულოა");
+                return;
+            }
+
+            using (StreamReader reader = new StreamReader("index.html"))
+            {
+                body = reader.ReadToEnd();
+            }
+            body = body.Replace("{fullname}", inp_fullname.Text.Trim());
+            body = body.Replace("{position}", inp_position.Text.Trim());
+            body = body.Replace("{department}", inp_department.Text.Trim());
+            body = body.Replace("{gankofileba}", inp_ganyofileba.Text.Trim());
+            body = body.Replace("{birthDay}", inp_birthDay.Value.ToString("d MMMM"));
+            body = body.Replace("{boss}", inp_boss.Text.Trim());
+            body = body.Replace("{startDate}", inp_startDate.Value.ToString("d MMMM"));
+            body = body.Replace("{email}", inp_email.Text.Trim());
+            body = body.Replace("{welcomeText}", inp_welcome_text.Text.Trim());
+            body = body.Replace("{welcomeText}", inp_welcome_text.Text.Trim());
+            body = body.Replace("{image}", imageStr);
+        }
+
+        private void btn_submit_Click(object sender, EventArgs e)
+        {
+            FillHtml();
+            SendMail();
+        }
+
+        private void btn_upload_Click(object sender, EventArgs e)
+        {
+            DialogResult dialog = openFileDialog1.ShowDialog();
+            if (dialog == DialogResult.OK)
+            {
+                var fileName = openFileDialog1.FileName;
+                ImageTobase64(fileName);
+            }
+        }
+
+        public void ImageTobase64(string imgPath)
+        {
+            byte[] imageBytes = System.IO.File.ReadAllBytes(imgPath);
+            string base64String = Convert.ToBase64String(imageBytes);
+            ResizeImage(Convert.FromBase64String(base64String));
+        }
+
+        //public void Base64ToImage(string image64)
+        //{
+        //    byte[] imageBytes = Convert.FromBase64String(image64);
+        //    ResizeImage(imageBytes);
+        //    using (var ms = new MemoryStream(imageBytes, 0, imageBytes.Length))
+        //    {
+        //        pictureBox1.Image = Image.FromStream(ms, true);
+        //    }
+        //}
+
+        private void btn_view_template_Click(object sender, EventArgs e)
+        {
+            FillHtml();
+            string path = Path.GetTempPath();
+            string fileName = Path.GetRandomFileName();
+            File.WriteAllText(path + fileName + ".html", body);
+            System.Diagnostics.Process.Start(path + fileName + ".html");
+        }
+
+        public void ResizeImage(byte[] imagebase64)
+        {
+            Image image;
+            using (MemoryStream ms = new MemoryStream(imagebase64))
+            {
+                image = Image.FromStream(ms);
+            }
+            Bitmap b = new Bitmap(300, 300);
+            Graphics g = Graphics.FromImage((Image)b);
+            g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+
+            g.DrawImage(image, 0, 0, 300, 300);
+            g.Dispose();
+            image = (Image)b;
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                image.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                byte[] imageBytes = ms.ToArray();
+                imageStr = Convert.ToBase64String(imageBytes);
+                pictureBox1.Image = Image.FromStream(ms, true);
+            }
+        }
+
+        public void SendMail()
+        {
+            try
+            {
+                Microsoft.Office.Interop.Outlook.Application app = new Microsoft.Office.Interop.Outlook.Application();
+                Microsoft.Office.Interop.Outlook.MailItem mailItem = app.CreateItem(Microsoft.Office.Interop.Outlook.OlItemType.olMailItem);
+                mailItem.Subject = $"ახალი თანამშრომელი {inp_fullname.Text.Trim()}";
+                mailItem.To = "albert.giulbaziani@lb.ge";
+                mailItem.HTMLBody = body;                
+                mailItem.Importance = Microsoft.Office.Interop.Outlook.OlImportance.olImportanceHigh;
+                mailItem.Display(false);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
